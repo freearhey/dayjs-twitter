7a2cf9ee7145d41268afa27696945a03c7686b03
Aleksandr Statciuk <free.arhey@gmail.com>
2021-08-26 02:01:12 +0300

    Update index.js

--- src/locale/index.js
+++ src/locale/index.js
@@ -1,7 +1,9 @@
-import en from "./en"
-import es from "./es"
-import ru from "./ru"
-import fr from "./fr"
-import el from "./el"
+import en from './en'
+import es from './es'
+import ru from './ru'
+import fr from './fr'
+import el from './el'
+import pt from './pt'
+import pt_br from './pt-br'
 
-export default { en, es, ru, fr, el }
+export default { en, es, ru, fr, el, pt, ['pt-br']: pt_br }
